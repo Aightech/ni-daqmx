abb061ec3a99dbb3a2918e31c620de122865ca32
aightech <long.devillard@gmail.com>
2023-02-02 17:47:11 +0000

    minor fix

--- src/main.cpp
+++ src/main.cpp
@@ -7,22 +7,24 @@ int main(void)
 	sensor.start_thread();
 
     int nb_channels = 6;
-    lsl::stream_info info_sample("pressure", "sample", nb_channels, 0,
+    lsl::stream_info info_sample("FT_sensor", "sample", nb_channels, 0,
                                  lsl::cf_double64);
     lsl::stream_outlet outlet_sample(info_sample);
     std::vector<double> sample(nb_channels);
     std::cout << "[INFOS] Now sending data... " << std::endl;
 
-    for(;;)
+    for(int i =0;;i++)
     {
 		if(sensor.has_new_data())
 		{
 			for(unsigned i = 0; i < nb_channels; i++)
 			{
 				sample[i] = sensor.get_FT(i);
-				printf("%.3f\t", sample[i]);
+				if(i%1000==0)
+				  printf("%.3f\t", sample[i]);
 			}
-			printf("\n");
+			if(i%1000==0)
+			  printf("\n");
 			outlet_sample.push_sample(sample);
 		}
     }
